41279e54e9a4f35f9b720fe28884bb2b801e8283
dzh <dzh_11@qq.com>
2019-01-30 01:38:41 +0800

    rm decompressor

--- tbcloud-node-httpproxy/src/main/java/tbcloud/node/httpproxy/NodeHttpServer.java
+++ tbcloud-node-httpproxy/src/main/java/tbcloud/node/httpproxy/NodeHttpServer.java
@@ -5,7 +5,6 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpContentDecompressor;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
@@ -112,7 +111,7 @@ class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
         p.addLast(new ReadTimeoutHandler(30));
         //p.addLast(new HttpContentCompressor()); //TODO
         p.addLast(new HttpRequestDecoder(4096, 8192, 8192, true));
-        p.addLast(new HttpContentDecompressor());
+        //p.addLast(new HttpContentDecompressor());
 
         //p.addLast(new HttpResponseEncoder());
 
