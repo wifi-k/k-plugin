bb99d96222454d9daec02fde13b74375a350bdf3
dzh <dzh_11@qq.com>
2019-02-22 20:23:50 +0800

    add log

--- tbcloud-node-httpproxy/src/main/java/tbcloud/node/httpproxy/http/HttpToTcpHandler.java
+++ tbcloud-node-httpproxy/src/main/java/tbcloud/node/httpproxy/http/HttpToTcpHandler.java
@@ -71,11 +71,11 @@ public class HttpToTcpHandler extends SimpleChannelInboundHandler<HttpObject> {
             ((HttpRequest) msg).headers().remove(HttpHeaderNames.PROXY_CONNECTION);
 
             // write header
-            ByteBuf buf = ctx.alloc().heapBuffer(1024, PacketConst.MAX_SIZE);
-            //ByteBuf buf = Unpooled.buffer(1024, PacketConst.MAX_SIZE);
+            //ByteBuf buf = ctx.alloc().heapBuffer(1024, PacketConst.MAX_SIZE);
+            ByteBuf buf = Unpooled.buffer(1024, PacketConst.MAX_SIZE);
             LOG.info("read buf {}", buf.readableBytes());
             // Encode the message.
-            HttpRequestEncoder_EXT HttpRequestEncoderExt = new HttpRequestEncoder_EXT();
+//            HttpRequestEncoder_EXT HttpRequestEncoderExt = new HttpRequestEncoder_EXT();
             HttpRequestEncoderExt.encodeInitialLine(buf, (HttpRequest) msg);
             HttpRequestEncoderExt.encodeHeaders(((HttpRequest) msg).headers(), buf);
             buf.writeBytes(CRLF);
