ad9364edd21362fd43c529cb1a4f6b3578385c75
dzh <dzh_11@qq.com>
2019-04-08 10:07:13 +0800

    add device is_online

--- tbcloud-user-api/src/main/java/tbcloud/user/api/http/req/NodeDeviceReq.java
+++ tbcloud-user-api/src/main/java/tbcloud/user/api/http/req/NodeDeviceReq.java
@@ -9,6 +9,34 @@ public class NodeDeviceReq extends NodeReq {
     private Integer isRecord;
     private Integer isBlock;
 
+    private String mac;
+    private String note;
+    private Integer IsOnline;
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Integer getIsOnline() {
+        return IsOnline;
+    }
+
+    public void setIsOnline(Integer isOnline) {
+        IsOnline = isOnline;
+    }
+
     public Integer getIsRecord() {
         return isRecord;
     }
